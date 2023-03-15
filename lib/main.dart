35d563100e6e9d55698d26e33976fad89e1e61aa
@Bahatimyk <manenomichael99@gmail.com>
2023-03-15 16:50:04 +0300

    Sidebar items

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var currentPage = DrawerSection.home;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,6 +52,124 @@ class _HomeState extends State<Home> {
   }
 
   Widget MyDrawerList() {
-    return Container();
+    return Container(
+      padding: EdgeInsets.only(
+        top: 15,
+      ),
+      child: Column(
+        children: [
+          menuitem(
+            1,
+            "Home",
+            Icons.home,
+            currentPage == DrawerSection.home ? true : false,
+          ),
+          menuitem(
+            2,
+            "Plan",
+            Icons.home,
+            currentPage == DrawerSection.plan ? true : false,
+          ),
+          menuitem(
+            3,
+            "Deposit",
+            Icons.home,
+            currentPage == DrawerSection.deposit ? true : false,
+          ),
+          menuitem(
+            4,
+            "Withdraw",
+            Icons.home,
+            currentPage == DrawerSection.withdraw ? true : false,
+          ),
+          menuitem(
+            5,
+            "Notification",
+            Icons.home,
+            currentPage == DrawerSection.notifications ? true : false,
+          ),
+          menuitem(
+            6,
+            "Statement",
+            Icons.home,
+            currentPage == DrawerSection.statement ? true : false,
+          ),
+          menuitem(
+            7,
+            "Account",
+            Icons.home,
+            currentPage == DrawerSection.account ? true : false,
+          ),
+        ],
+      ),
+    );
   }
+
+  Widget menuitem(int id, String title, IconData icon, bool selected) {
+    return Material(
+      color: selected ? Colors.grey[300] : Colors.transparent,
+      child: InkWell(
+        onTap: () {
+          Navigator.pop(context);
+          setState(() {
+            if (id == 1) {
+              currentPage = DrawerSection.home;
+            }
+            if (id == 2) {
+              currentPage = DrawerSection.plan;
+            }
+            if (id == 3) {
+              currentPage = DrawerSection.deposit;
+            }
+            if (id == 4) {
+              currentPage = DrawerSection.withdraw;
+            }
+            if (id == 5) {
+              currentPage = DrawerSection.notifications;
+            }
+            if (id == 6) {
+              currentPage = DrawerSection.statement;
+            }
+            if (id == 7) {
+              currentPage = DrawerSection.account;
+            }
+          });
+        },
+        child: Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: Icon(
+                  icon,
+                  size: 20,
+                  color: Colors.black,
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Text(
+                  title,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+enum DrawerSection {
+  home,
+  plan,
+  deposit,
+  withdraw,
+  notifications,
+  statement,
+  account,
 }
